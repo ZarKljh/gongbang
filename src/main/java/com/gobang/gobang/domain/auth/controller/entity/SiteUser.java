906d0c6780e1d89177e4ed9cfedc6de50b07e661
ZarKljh <zarkljh@gmail.com>
2025-10-17 22:02:39 +0900

    entity adding

--- src/main/java/com/gobang/gobang/domain/auth/controller/entity/SiteUser.java
+++ src/main/java/com/gobang/gobang/domain/auth/controller/entity/SiteUser.java
@@ -1,6 +1,8 @@
 package com.gobang.gobang.domain.auth.controller.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
@@ -14,7 +16,24 @@ import lombok.experimental.SuperBuilder;
 @ToString(callSuper = true)
 public class SiteUser {
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
 
-    private String username;
+    /**
+     Long id
+     String email
+     String password
+     String userName
+     String phone
+     String nickName
+     String role
+     LocalDateTime createDate
+     LocalDateTime updateDate
+     String status
+     Long profileImgId
+     LocalDateTime deletedDate
+     LocalDateTime birthDay
+     String refreshToken
+     String gender
+    */
 }
