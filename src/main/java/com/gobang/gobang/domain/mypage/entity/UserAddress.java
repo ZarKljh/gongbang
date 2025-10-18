7d7bbe7c9298481109895e00fc0dfa24b7f9d91f
jiseon723 <dighxxx12@gmail.com>
2025-10-18 14:37:47 +0900

    entity

---
+++ src/main/java/com/gobang/gobang/domain/mypage/entity/UserAddress.java
@@ -0,0 +1,50 @@
+package com.gobang.gobang.domain.mypage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_address")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_address_id")
+    private Long userAddressId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "recipient_name", nullable = false, length = 50)
+    private String recipientName;
+
+    @Column(name = "base_address", nullable = false, length = 10)
+    private String baseAddress;
+
+    @Column(name = "detail_address", nullable = false, length = 255)
+    private String detailAddress;
+
+    @Column(name = "zipcode", nullable = false, length = 255)
+    private String zipcode;
+
+    @Column(name = "is_default", nullable = false)
+    private Boolean isDefault = false;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/gobang/gobang/domain/mypage/entity/WishList.java
@@ -0,0 +1,32 @@
+package com.gobang.gobang.domain.mypage.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "wish_list")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WishList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "wishlist_id")
+    private Long wishlistId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
