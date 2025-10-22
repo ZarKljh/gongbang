7b2506e7e75665b0302c7d6456d3b45ec3a413f7
0n0ng <hy991006@gmail.com>
2025-10-22 18:05:00 +0900

    front 리뷰 등록

--- src/main/java/com/gobang/gobang/domain/review/dto/ReviewDto.java
+++ src/main/java/com/gobang/gobang/domain/review/dto/ReviewDto.java
@@ -1,6 +1,7 @@
 package com.gobang.gobang.domain.review.dto;
 
 import com.gobang.gobang.domain.review.entity.Review;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
