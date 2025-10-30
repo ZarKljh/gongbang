b091fdcd4c42c5825d8137a359d3a6297d608eb2
ZarKljh <zarkljh@gmail.com>
2025-10-30 18:40:16 +0900

    studioViewPage add productList

---
+++ src/main/java/com/gobang/gobang/domain/seller/repository/ProductOfStudioRepository.java
@@ -0,0 +1,16 @@
+package com.gobang.gobang.domain.seller.repository;
+
+import com.gobang.gobang.domain.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+//커스텀 리포지터리
+public interface ProductOfStudioRepository extends JpaRepository<Product, Long> {
+    @Query(
+        value = "SELECT * FROM product WHERE studio_id = :studioId",
+        countQuery = "SELECT COUNT(*) FROM product WHERE studio_id = :studioId",
+        nativeQuery = true
+    )
+    Page<Product> findByStudioId(Long studioId, Pageable pageable);
+}
