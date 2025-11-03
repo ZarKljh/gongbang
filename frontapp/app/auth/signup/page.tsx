0ed02f8c7d1a7ed14c4b413e5a944b7605e7a1be
ZarKljh <zarkljh@gmail.com>
2025-11-03 18:50:05 +0900

    signup login front update

--- frontapp/app/auth/signup/page.tsx
+++ frontapp/app/auth/signup/page.tsx
@@ -4,9 +4,19 @@ import './signup.css'
 export default function SignupSelector() {
     return (
         <div className="signup-container">
-            <h3>회원가입유형을 선택해주세요</h3>
-            <Link href="/auth/signup/user">일반사용자 회원가입</Link>
-            <Link href="/auth/signup/seller">공방사업자 회원가입</Link>
+            <h3 className="signup-title">회원가입유형을 선택해주세요</h3>
+            <div className="signup-group">
+                <div className="signup-box signup-user-icon">
+                    <Link href="/auth/signup/user">
+                        <span>일반사용자</span>
+                    </Link>
+                </div>
+                <div className="signup-box signup-seller-icon">
+                    <Link href="/auth/signup/seller">
+                        <span>공방사업자</span>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
--- frontapp/app/auth/signup/user/page.tsx
+++ frontapp/app/auth/signup/user/page.tsx
@@ -56,110 +56,103 @@ export default function SignupUser() {
     return (
         <section className="signup-container">
             <h3 className="signup-title">일반회원 회원가입페이지</h3>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label className="form-label">ID</label>
-                    <input
-                        type="text"
-                        name="userName"
-                        className="form-input"
-                        onChange={handleChange}
-                        value={formData.userName}
-                        placeholder="로그인에 사용되는 ID입니다"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">패스워드</label>
-                    <input
-                        type="password"
-                        name="password"
-                        className="form-input"
-                        onChange={handleChange}
-                        value={formData.password}
-                        placeholder="패스워드"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">패스워드확인</label>
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        className="form-input"
-                        onChange={handleChange}
-                        value={formData.confirmPassword}
-                        placeholder="패스워드를 다시 입력해주세요"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">이메일</label>
-                    <input
-                        type="text"
-                        name="email"
-                        className="form-input"
-                        onChange={handleChange}
-                        value={formData.email}
-                        placeholder="소문자로입력해주세요"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">성명</label>
-                    <input
-                        type="text"
-                        name="fullName"
-                        className="form-input"
-                        onChange={handleChange}
-                        value={formData.fullName}
-                        placeholder="성명을 한글로 적어주세요"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-lable">성별</label>
-                    <select name="gender" value={formData.gender} onChange={handleChange}>
-                        <option value="" disabled>
-                            성별을 선택해주세요
-                        </option>
-                        <option value="M">남성</option>
-                        <option value="F">여성</option>
-                    </select>
-                </div>
-                <div className="form-group">
-                    <label className="form-label">생년월일</label>
-                    <input
-                        type="date"
-                        name="birth"
-                        className="form-input"
-                        value={formData.birth}
-                        onChange={handleChange}
-                        min="1900-01-01"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">닉네임</label>
-                    <input
-                        type="text"
-                        name="nickName"
-                        className="form-input"
-                        value={formData.nickName}
-                        onChange={handleChange}
-                        placeholder="50자이내로 적어주세요"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="form-label">휴대전화</label>
-                    <input
-                        type="text"
-                        name="mobilePhone"
-                        className="form-input"
-                        value={formData.mobilePhone}
-                        onChange={handleChange}
-                        placeholder="번호만적어주세요"
-                    />
-                </div>
-                <div className="button-group">
-                    <input type="submit" className="btn btn-primary" value="저장" />
-                    {/* <button type="submit">등록</button> */}
-                </div>
-            </form>
+            <div className="form-container">
+                <h4 className="form-title">사용자 정보 입력</h4>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label className="form-label">ID</label>
+                        <input
+                            type="text"
+                            name="userName"
+                            className="form-input"
+                            onChange={handleChange}
+                            value={formData.userName}
+                            placeholder="로그인에 사용되는 ID입니다"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">패스워드</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="form-input"
+                            onChange={handleChange}
+                            value={formData.password}
+                            placeholder="패스워드"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">패스워드확인</label>
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            className="form-input"
+                            onChange={handleChange}
+                            value={formData.confirmPassword}
+                            placeholder="패스워드를 다시 입력해주세요"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">성명</label>
+                        <input
+                            type="text"
+                            name="fullName"
+                            className="form-input"
+                            onChange={handleChange}
+                            value={formData.fullName}
+                            placeholder="성명을 한글로 적어주세요"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">이메일</label>
+                        <input
+                            type="text"
+                            name="email"
+                            className="form-input"
+                            onChange={handleChange}
+                            value={formData.email}
+                            placeholder="소문자로입력해주세요"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">생년월일</label>
+                        <input
+                            type="date"
+                            name="birth"
+                            className="form-input"
+                            value={formData.birth}
+                            onChange={handleChange}
+                            min="1900-01-01"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">닉네임</label>
+                        <input
+                            type="text"
+                            name="nickName"
+                            className="form-input"
+                            value={formData.nickName}
+                            onChange={handleChange}
+                            placeholder="50자이내로 적어주세요"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label className="form-label">휴대전화</label>
+                        <input
+                            type="text"
+                            name="mobilePhone"
+                            className="form-input"
+                            value={formData.mobilePhone}
+                            onChange={handleChange}
+                            placeholder="번호만적어주세요"
+                        />
+                    </div>
+                    <div className="button-group">
+                        <input type="submit" className="btn btn-primary" value="저장" />
+                        {/* <button type="submit">등록</button> */}
+                    </div>
+                </form>
+            </div>
         </section>
     )
 }
