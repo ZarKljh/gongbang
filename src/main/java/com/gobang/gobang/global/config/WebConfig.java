99746204cc62b4cf9f9460883d2a8db29850e1f9
ZarKljh <zarkljh@gmail.com>
2025-11-21 17:40:51 +0900

    feature rebase

--- src/main/java/com/gobang/gobang/global/config/WebConfig.java
+++ src/main/java/com/gobang/gobang/global/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
