3af392552b9cec234f887f31f65d7f749ceca960
ZarKljh <zarkljh@gmail.com>
2025-12-17 13:42:38 +0900

    merge personal auth into feature

--- src/main/java/com/gobang/gobang/global/config/WebConfig.java
+++ src/main/java/com/gobang/gobang/global/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,10 +12,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-
-import java.time.Duration;
-
 
 import java.util.List;
 
