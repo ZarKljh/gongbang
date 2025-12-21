2453385c07c8d93b02378f325eb4ff2a75a2afac
hyo <rlagywnd05@gmail.com>
2025-12-21 16:37:00 +0900

    개발테스트용도로 소스변경

--- src/main/java/com/gobang/gobang/global/config/WebConfig.java
+++ src/main/java/com/gobang/gobang/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,6 +13,9 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+import java.time.Duration;
 
 
 @Configuration
